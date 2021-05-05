e0fb71310478bacea143e999c7bb9475e4a6ef7a
RajanKumar5 <rajan.kumar.rk.0502@gmail.com>
2021-05-05 12:38:03 +0530

    created constant questions

--- app/src/main/java/com/example/quizapp/QuizQuestions.kt
+++ app/src/main/java/com/example/quizapp/QuizQuestions.kt
@@ -2,10 +2,19 @@ package com.example.quizapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.example.quizapp.databinding.ActivityQuizQuestionsBinding
 
 class QuizQuestions : AppCompatActivity() {
+
+    private lateinit var binding: ActivityQuizQuestionsBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityQuizQuestionsBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_quiz_questions)
+        setContentView(binding.root)
+
+        val questionsList = Constants.getQuestions()
+        Log.i("Question Size", "${questionsList.size}")
     }
 }
