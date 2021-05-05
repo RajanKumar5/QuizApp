4fe827ea52544bd6f4cce41ea931f10d5de06c89
RajanKumar5 <rajan.kumar.rk.0502@gmail.com>
2021-05-05 23:32:50 +0530

    added functionality in QuizQuestions.kt & activity_quiz_questions.xml

--- app/src/main/java/com/example/quizapp/QuizQuestions.kt
+++ app/src/main/java/com/example/quizapp/QuizQuestions.kt
@@ -1,20 +1,109 @@
 package com.example.quizapp
 
+import android.graphics.Color
+import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.TextView
+import androidx.core.content.ContextCompat
 import com.example.quizapp.databinding.ActivityQuizQuestionsBinding
+import org.w3c.dom.Text
 
-class QuizQuestions : AppCompatActivity() {
+class QuizQuestions : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var binding: ActivityQuizQuestionsBinding
+
+    private var mCurrentPosition: Int = 1
+    private var mQuestionsList: ArrayList<Question>? = null
+    private var mSelectedOptionPosition: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivityQuizQuestionsBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-        val questionsList = Constants.getQuestions()
-        Log.i("Question Size", "${questionsList.size}")
+        mQuestionsList = Constants.getQuestions()
+
+        setQuestion()
+
+        binding.tvOptionOne.setOnClickListener(this)
+        binding.tvOptionTwo.setOnClickListener(this)
+        binding.tvOptionThree.setOnClickListener(this)
+        binding.tvOptionFour.setOnClickListener(this)
+
+        /*binding.btnSubmit.setOnClickListener {
+            if(mCurrentPosition < 10){
+                mCurrentPosition += 1
+            }
+            setQuestion()
+        }*/
+
     }
+
+    private fun setQuestion() {
+        mCurrentPosition = 1
+        val question = mQuestionsList!![mCurrentPosition - 1]
+
+        defaultOptionsView()
+
+        binding.progressBar.progress = mCurrentPosition
+        binding.ivImage.setImageResource(question!!.image)
+        binding.tvProgressBar.text = "${mCurrentPosition}/${binding.progressBar.max}"
+
+        binding.tvQuestion.text = question!!.question
+        binding.tvOptionOne.text = question!!.optionOne
+        binding.tvOptionTwo.text = question!!.optionTwo
+        binding.tvOptionThree.text = question!!.optionThree
+        binding.tvOptionFour.text = question!!.optionFour
+    }
+
+    private fun defaultOptionsView() {
+        val options = ArrayList<TextView>()
+        options.add(0, binding.tvOptionOne)
+        options.add(1, binding.tvOptionTwo)
+        options.add(2, binding.tvOptionThree)
+        options.add(3, binding.tvOptionFour)
+
+        for (option in options) {
+            option.setTextColor(Color.parseColor("#7A8089"))
+            option.typeface = Typeface.DEFAULT
+            option.background = ContextCompat.getDrawable(
+                this,
+                R.drawable.default_option_border_bg
+            )
+        }
+    }
+
+    private fun selectedOptionView(tv: TextView, selectedOptionNumber: Int) {
+        defaultOptionsView()
+        mSelectedOptionPosition = selectedOptionNumber
+
+        tv.setTextColor(Color.parseColor("#363A43"))
+        tv.typeface = Typeface.DEFAULT_BOLD
+        tv.background = ContextCompat.getDrawable(
+            this,
+            R.drawable.selected_option_border_bg
+        )
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.tvOptionOne -> {
+                selectedOptionView(binding.tvOptionOne, 1)
+            }
+            R.id.tvOptionTwo -> {
+                selectedOptionView(binding.tvOptionTwo, 2)
+            }
+            R.id.tvOptionThree -> {
+                selectedOptionView(binding.tvOptionThree, 3)
+            }
+            R.id.tvOptionFour -> {
+                selectedOptionView(binding.tvOptionFour, 4)
+            }
+
+        }
+    }
+
 }
