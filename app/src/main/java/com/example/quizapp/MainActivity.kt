160ef6e796a939f83be58fdb1cc7203a1ad0ad56
RajanKumar5 <rajan.kumar.rk.0502@gmail.com>
2021-05-05 12:03:45 +0530

    updated MainActivity.kt and activity_main.xml

--- app/src/main/java/com/example/quizapp/MainActivity.kt
+++ app/src/main/java/com/example/quizapp/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.quizapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import com.example.quizapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,6 +15,18 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+
+        binding.btnStart.setOnClickListener {
+            if (binding.tvName.text.toString().isEmpty()) {
+                Toast.makeText(this, "Please enter your name", Toast.LENGTH_LONG).show()
+            } else {
+                val intent = Intent(this, QuizQuestions::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }
 
     }
 }
